3cf2c655f8c60fc12fab4f806de25d137ebb3cd5
lucasGabrielDeAA <lg.technus@gmail.com>
2021-02-22 20:05:45 -0300

    src added

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { Container, Label } from './styles'
+
+export default function Home() {
+
+  return (
+    <Container>
+      <Label>Hello everyone!</Label>
+    </Container>
+  )
+
+}
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.View`
+  align-items: center;
+  background-color: #fff;
+  flex: 1;
+  justify-content: center;
+`
+
+export const Label = styled.Text`
+  color: #000;
+  font-size: 16px;
+`
