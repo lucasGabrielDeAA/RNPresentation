23f1f62b7101d1479e35c350f1d9aa51a24b4ff4
lucasGabrielDeAA <lg.technus@gmail.com>
2021-02-22 22:38:59 -0300

    visual changes

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-import { Container, Label } from './styles'
+import { Container, Label, Button, ButtonLabel } from './styles'
 
 export default function Home() {
 
   return (
     <Container>
       <Label>Hello everyone!</Label>
+
+      <Button onPress={() => {}}>
+        <ButtonLabel>Click me</ButtonLabel>
+      </Button>
     </Container>
   )
 
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -11,3 +11,19 @@ export const Label = styled.Text`
   color: #000;
   font-size: 16px;
 `
+export const Button = styled.TouchableOpacity`
+  align-items: center;
+  background-color: #000;
+  border-radius: 5px;
+  elevation: 5;
+  justify-content: center;
+  margin: 16px;
+  padding: 4px 8px;
+  shadow-offset: { height: 2px, width: 0 };
+  shadow-opacity: 0.2;
+`
+
+export const ButtonLabel = styled.Text`
+  color: #fff;
+  font-size: 16px;
+`
