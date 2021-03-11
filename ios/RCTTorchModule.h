04664608914fdb90fd41ac858978b0e7dc72bab7
lucasGabrielDeAA <lg.technus@gmail.com>
2021-03-11 14:28:19 -0300

    IOS module instructions

--- ios/RCTTorchModule.h
+++ ios/RCTTorchModule.h
@@ -4,6 +4,7 @@
 //
 //  Created by Lucas Gabriel de Araújo Assis on 02/03/21.
 //
+// This file works is the header of our native module on IOS's environment, do you remember the .h files on C, and C++ software?
 #import <React/RCTBridgeModule.h>
 
 @interface RCTTorchModule : NSObject <RCTBridgeModule>
--- ios/RCTTorchModule.m
+++ ios/RCTTorchModule.m
@@ -4,18 +4,22 @@
 //
 //  Created by Lucas Gabriel de Araújo Assis on 02/03/21.
 //
-
+// This file provides the implementation of our .h file, and is the declaration of our module, and it methods to be provided to JS code level
 #import <AVFoundation/AVFoundation.h>
 #import "RCTTorchModule.h"
 
 @implementation RCTTorchModule
 
+
+// NativeModules bridge name, the same approach used on Java, with the getName method.
+// The main difeerence between this both methods is the non-literal string usage here in obj-c code, like "TorchLight"
 RCT_EXPORT_MODULE(TorchLight)
 
+// Methods to be used on JS code level, need this signature on obj-C code level, the same approach when we use @ReactMethod on Java code
 RCT_EXPORT_METHOD(switchTorch: (BOOL *) onOffTorch) {
     if ([AVCaptureDevice class]) {
         AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-        
+
         if ([device hasTorch]) {
             [device lockForConfiguration:nil];
 
@@ -36,14 +40,14 @@ RCT_EXPORT_METHOD(hasFlashLight:(RCTPromiseResolveBlock)resolve rejecter:(RCTPro
 
         if ([AVCaptureDevice class]) {
             AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-            
+
             if ([device hasTorch]) {
                 isFlashAvailable = true;
             } else {
                 isFlashAvailable = false;
             }
         }
-      
+
       resolve([NSNumber numberWithBool:isFlashAvailable]);
     } @catch (NSException *exception) {
         reject(@"HAS_FLASH_LIGHT_ERROR", @"", nil);
