e259f74ff5bc4dbc35bc512692d91635e1e2feb5
lucasGabrielDeAA <lg.technus@gmail.com>
2021-03-16 22:38:00 -0300

    Adjustments

--- ios/RCTTorchModule.m
+++ ios/RCTTorchModule.m
@@ -12,16 +12,18 @@
 
 
 // NativeModules bridge name, the same approach used on Java, with the getName method.
-// The main difference between this both methods is the non-literal string usage here in obj-c code, like "TorchLight"
+// The main difference between these two methods is the non-literal string usage here in obj-c code
 RCT_EXPORT_MODULE(TorchLight)
 
 // Methods to be used on JS code level, need this signature on obj-C code level, the same approach when we use @ReactMethod on Java code
 RCT_EXPORT_METHOD(switchTorch: (BOOL *) onOffTorch) {
   // Checking if the device has a camera device
     if ([AVCaptureDevice class]) {
-      // Checking the flash
+      
+        // Getting the camera
         AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
 
+        // Checking the flash
         if ([device hasTorch]) {
             [device lockForConfiguration:nil];
 
